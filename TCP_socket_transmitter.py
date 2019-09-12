117c43f0e557aecc1c27df75966801d6bcab7505
BMH <bijanmehralizadeh@gmail.com>
2019-09-12 12:40:34 +0430

    Update TCP_socket_transmitter.py

--- TCP_socket_transmitter.py
+++ TCP_socket_transmitter.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
 
 import socket
+import time
+
+def sender(msg):
+    TCP_IP = '192.168.1.100'
+    TCP_PORT = 80
+    BUFFER_SIZE = 1024
+    # msg = bytes(msg, 'utf-8')
+    MESSAGE = msg
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+
+    print("received data:", data)
 
 
-TCP_IP = '192.168.1.100'
-TCP_PORT = 80
-BUFFER_SIZE = 1024
-MESSAGE = bytes("MR-160L-110*", 'utf-8')
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
-
-print("received data:", data)
+if __name__ == "__main__":
+    sender("MR-1023L-1023*")
+    # time.sleep(1)
+    # sender("MR-1024L1024*")
+    # time.sleep(1)
+    # sender("MR0L0*")
+    # time.sleep(1)
+    # sender("MR1023L1023*")
+    # time.sleep(1)
+    # sender("MR0L0*")
+    
